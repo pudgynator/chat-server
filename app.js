9b509ed040f4ced46920a04ac312b1d326ed3041
Maria <dobrydnyk.m@gmail.com>
2026-07-07 17:35:18 +0300

    Update dependencies

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from 'cors';
+
 const PORT = process.env.PORT || 3000;
-
-
+const corsOptions = {
+    origin: ['http://localhost:5173'],
+};
 const app = express();
+
+app.use(cors(corsOptions))
 
 app.get('/api', (req, res) => {
     res.send('hello');
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
