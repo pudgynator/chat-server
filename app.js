7a33a500ac0311dcd57e78d594b572d0c43463fd
Maria <dobrydnyk.m@gmail.com>
2026-07-07 17:11:22 +0300

    Initialize server

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.send('hello');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
