58968b0356d57f907b79993a2565381526e9d12b
Maria <dobrydnyk.m@gmail.com>
2026-07-13 13:24:47 +0300

    Update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 import authRouter from './authRoute.js';
+import chatsRouter from './chatsRoute.js';
 
 router.use('/', authRouter);
+router.use('/api/chats', chatsRouter);
 
 export default router;
