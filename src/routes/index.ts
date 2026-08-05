7bf277b8236b079642d67bd904ebc3ab878d9e8e
Maria <dobrydnyk.m@gmail.com>
2026-08-05 12:00:58 +0300

    Update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,10 +5,12 @@ import authRouter from './authRoute.js';
 import chatsRouter from './chatsRoute.js';
 import contactsRouter from './contactsRoute.js';
 import messagesRouter from './messagesRoute.js'
+import userRouter from './userRoute.js'
 
 router.use('/', authRouter);
 router.use('/api/chats', chatsRouter);
 router.use('/api/contacts', contactsRouter);
 router.use('/api/messages', messagesRouter)
+router.use('/api/profile', userRouter);
 
 export default router;
